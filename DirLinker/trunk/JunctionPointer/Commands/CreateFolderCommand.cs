49b40854ddd2bb940264f48d60a8f89fe867042f
Will <devnull@localhost>
2010-02-24 23:01:39 +0000

    renamed create folder

---
+++ DirLinker/trunk/JunctionPointer/Commands/CreateFolderCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using JunctionPointer.Interfaces;
+
+namespace JunctionPointer.Commands
+{
+    public class CreateFolderCommand : ICommand
+    {
+        
+        public void Execute()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Undo()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Status
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public event RequestUserReponse AskUser;
+    }
+}
