ae31dea6622eedae70d882fc3e5d9bf729f6f3da
Will <devnull@localhost>
2010-04-22 07:27:29 +0100

    Added Delete file command with tests

---
+++ DirLinker/trunk/JunctionPointer/Commands/DeleteFileCommand.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DirLinker.Interfaces;
+using System.IO;
+
+namespace DirLinker.Commands
+{
+    public class DeleteFileCommand : ICommand
+    {
+        private IFile _fileToDelete;
+        public DeleteFileCommand(IFile fileToDelete)
+        {
+            _fileToDelete = fileToDelete;
+        }
+
+        public void Execute()
+        {
+            if (_fileToDelete.Exists())
+            {
+                CheckFilePermissions();
+                _fileToDelete.Delete();
+            }
+        }
+
+        private void CheckFilePermissions()
+        {
+            if ((_fileToDelete.GetAttributes() & FileAttributes.ReadOnly) == FileAttributes.ReadOnly)
+            {
+                _fileToDelete.SetAttributes(FileAttributes.Normal);
+            }
+        }
+
+        public void Undo()
+        {
+           
+        }
+
+        public string UserFeedback
+        {
+            get { return String.Format("Deleting file {0}", _fileToDelete.FullFilePath); }
+        }
+
+        public event RequestUserReponse AskUser;
+
+    }
+}
