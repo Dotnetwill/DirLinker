f1defbb448e7d89eacdad7e759d779c10f4e7056
Will <devnull@localhost>
2010-05-04 19:19:36 +0100

    Added operating system detection code

---
+++ DirLinker/trunk/JunctionPointer/Implementation/OperatingSystemVersion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DirLinker.Interfaces;
+
+namespace DirLinker.Implementation
+{
+    class OperatingSystemVersion : IOperatingSystemVersion
+    {
+        public Boolean IsXp()
+        {
+            var osVersion = Environment.OSVersion;
+            return osVersion.Version.Major == 5 && osVersion.Version.Minor > 0;
+        }
+
+        public Boolean IsVistaOrLater()
+        {
+            var osVersion = Environment.OSVersion;
+            return osVersion.Version.Major >= 6;
+        }
+    }
+}
---
+++ DirLinker/trunk/JunctionPointer/Interfaces/IOperatingSystemVersion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DirLinker.Interfaces
+{
+    public interface IOperatingSystemVersion
+    {
+        Boolean IsVistaOrLater();
+        Boolean IsXp();
+    }
+}
