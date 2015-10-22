036896b038769c74652653ea7fbb956348410453
DiligentKeyPresser <tkachev.daniil@gmail.com>
2015-10-22 19:54:47 +1000

    (c) :)

--- ElevatedWorker/Properties/AssemblyInfo.cs
+++ ElevatedWorker/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ElevatedWorker")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © Diligent Key Presser 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
